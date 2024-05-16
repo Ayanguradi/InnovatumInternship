3b4ba9e27b9053e8fef20d1f2d07b84a72a79979
Ayanguradi <ayanguradi2@gmail.com>
2024-05-16 17:18:00 +0530

    python basics

---
+++ python/python_day1.py
@@ -0,0 +1,32 @@
+# import random
+#
+# # Define responses to different types of inputs
+# responses = {
+#     "hi": ["Hello!", "Hi there!", "Hey!"],
+#     "how are you": ["I'm doing well, thank you!", "I'm good, thanks for asking!"],
+#     "bye": ["Goodbye!", "See you later!", "Bye!"],
+#     "default": ["I'm not sure I understand.", "Could you please rephrase that?", "I'm still learning!"]
+# }
+#
+# # Function to generate responses
+# def respond(input_text):
+#     input_text = input_text.lower()
+#     if input_text in responses:
+#         return random.choice(responses[input_text])
+#     else:
+#         return random.choice(responses["default"])
+#
+# # Main loop to interact with the user
+# print("Welcome to the Simple Chatbot!")
+# print("You can start chatting with me. Type 'bye' to exit.")
+#
+# while True:
+#     user_input = input("You: ")
+#     if user_input.lower() == 'bye':
+#         print("Bot: " + respond(user_input))
+#         break
+#     else:
+#         print("Bot: " + respond(user_input))
+
+print("hi ,hello")
+
